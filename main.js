74410899ae0a67fa6f3ed0dacd9f1710ac9c654a
brahim-derouiche <derouichebrahim66@gmail.com>
2020-07-14 08:51:19 +0100

    done

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var myList = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myList.length; i++) {
   var tag = document.createElement("SPAN");
-  var txt = document.createTextNode("X");
+  var txt = document.createTextNode("❌");
   tag.className = "close";
   tag.appendChild(txt);
   myList[i].appendChild(tag);
@@ -16,11 +16,10 @@ for (i = 0; i < close.length; i++) {
     div.style.display = "none";
   }
 }
-//ev is the actual click event itself which would tell you if the user clicked with the right mouse button or left mouse button
 var toDo = document.querySelector('ul');
-toDo.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+toDo.addEventListener('click', function(click) {
+  if (click.target.tagName === 'LI') {
+    click.target.classList.toggle('checked');
   }
 }, false);
 
@@ -37,7 +36,7 @@ function newElement() {
   document.getElementById("myInput").value = "";
 
   var tag = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  var txt = document.createTextNode("❌");
   tag.className = "close";
   tag.appendChild(txt);
   li.appendChild(tag);
